370e458c1e1cda4786424330659cccb0766eb2b5
ark <jaylua2548@gmail.com>
2025-09-09 01:41:17 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,19 +1,16 @@
-return function()
-    local Lighting = game:GetService("Lighting")
+local Lighting = game:GetService("Lighting")
 
-    local sky = Instance.new("Sky")
-    sky.SkyboxBk = "rbxassetid://127331257508011"
-    sky.SkyboxDn = "rbxassetid://127331257508011"
-    sky.SkyboxFt = "rbxassetid://127331257508011"
-    sky.SkyboxLf = "rbxassetid://127331257508011"
-    sky.SkyboxRt = "rbxassetid://127331257508011"
-    sky.SkyboxUp = "rbxassetid://127331257508011"
-
-    for _, v in ipairs(Lighting:GetChildren()) do
-        if v:IsA("Sky") then
-            v:Destroy()
-        end
+for _, v in ipairs(Lighting:GetChildren()) do
+    if v:IsA("Sky") then
+        v:Destroy()
     end
-
-    sky.Parent = Lighting
 end
+
+local sky = Instance.new("Sky")
+sky.SkyboxBk = "rbxassetid://127331257508011"
+sky.SkyboxDn = "rbxassetid://127331257508011"
+sky.SkyboxFt = "rbxassetid://127331257508011"
+sky.SkyboxLf = "rbxassetid://127331257508011"
+sky.SkyboxRt = "rbxassetid://127331257508011"
+sky.SkyboxUp = "rbxassetid://127331257508011"
+sky.Parent = Lighting
